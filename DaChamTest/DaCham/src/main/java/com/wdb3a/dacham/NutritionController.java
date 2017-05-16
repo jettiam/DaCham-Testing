920fc43a587daf170dd9f99c9a9472aa5ec49b66
WDB <hhj5828@naver.com>
2017-05-17 08:19:38 +0900

    황재웅 - 영양사 컨트롤러 주석 추가

--- DaChamTest/DaCham/src/main/java/com/wdb3a/dacham/NutritionController.java
+++ DaChamTest/DaCham/src/main/java/com/wdb3a/dacham/NutritionController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 /*
- * 고객페이지 컨트롤러
+ * 영양사페이지 컨트롤러
  */
 @Controller
 @RequestMapping("mate/nutritionist")
