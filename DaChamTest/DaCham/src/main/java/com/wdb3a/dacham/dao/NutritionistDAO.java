260e0c5451ebb28c6ef823cadd868bbe37d7a9f5
WDB <hhj5828@naver.com>
2017-06-15 23:29:26 +0900

    황재웅 - 식단 등록 페이지의 반찬 검색  ajax로 변경

--- DaChamTest/DaCham/src/main/java/com/wdb3a/dacham/dao/NutritionistDAO.java
+++ DaChamTest/DaCham/src/main/java/com/wdb3a/dacham/dao/NutritionistDAO.java
@@ -5,7 +5,10 @@ import java.util.List;
 import com.wdb3a.dacham.bean.Nutritionist;
 
 public interface NutritionistDAO {
+//	반찬 목록을 검색합니다.
 	public List<Nutritionist> listSearch(Nutritionist nutritionist) throws Exception;
+// 반찬 전체를 조회합니다.(with 식품군, 조리법)
+	public List<Nutritionist> sideAll() throws Exception;
 	public List<Nutritionist> materialSearch(Nutritionist nutritionist) throws Exception;
 	public Nutritionist materialView(String sideDCode) throws Exception;
 	public List<Nutritionist> materialAll() throws Exception;
@@ -18,4 +21,6 @@ public interface NutritionistDAO {
 	public void createDietInfo(Nutritionist nutritionist) throws Exception;
 	public Nutritionist showKcal(String sideDCode) throws Exception;
 	public List<Nutritionist> optionTemplate(Nutritionist nutritionist) throws Exception;
+//	반찬 템플릿의 개수를 구합니다.
+	public int optionTemplateCount(Nutritionist nutritionist) throws Exception;
 }
