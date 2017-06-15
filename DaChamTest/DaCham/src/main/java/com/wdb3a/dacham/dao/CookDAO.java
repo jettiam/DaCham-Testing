7f1ad4a97b5011816aa22a8e79a57d3f836b672b
bon <bon@bon300-12>
2017-06-15 17:44:33 +0900

    양정석 - 조리페이지 구현(50%), 각 구역 DB출력,레이아웃 약간 수정

--- DaChamTest/DaCham/src/main/java/com/wdb3a/dacham/dao/CookDAO.java
+++ DaChamTest/DaCham/src/main/java/com/wdb3a/dacham/dao/CookDAO.java
@@ -9,4 +9,7 @@ import com.wdb3a.dacham.bean.FoodMInven;
 public interface CookDAO {
 	public List<FoodMInven> CookStock(FoodMInven foodInv) throws Exception;
 	public List<Cook> cookList() throws Exception;
+	public List<Cook> readycook() throws Exception;
+	public List<Cook> cookingfood() throws Exception;
+	public List<Cook> finishcook() throws Exception;
 }
