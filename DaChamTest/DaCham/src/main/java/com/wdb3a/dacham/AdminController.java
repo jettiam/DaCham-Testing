c59f28031df2a67bfe8742c31ac5d51ba2251085
bon <bon@bon300-11>
2017-08-20 12:35:03 +0900

    조영삼 : 커밋용

--- DaChamTest/DaCham/src/main/java/com/wdb3a/dacham/AdminController.java
+++ DaChamTest/DaCham/src/main/java/com/wdb3a/dacham/AdminController.java
@@ -244,7 +244,7 @@ public class AdminController {
 			messageHelper.setText(content); // 메일 내용
 
 			mailSender.send(message);
-		} catch (Exception e) {
+		} catch (Exception e) { 
 			System.out.println(e);
 		}
 		
@@ -307,9 +307,9 @@ public class AdminController {
 		 * messageHelper.setSubject(title); // 메일제목은 생략이 가능하다
 		 * messageHelper.setText(content); // 메일 내용
 		 * 
-		 * mailSender.send(message); } catch(Exception e){
+		 * mailSender.send(message); } catch(Exception e){ 
 		 * System.out.println(e); }
-		 */
+		 */  
 
 		return "redirect:orderList";
 	}
